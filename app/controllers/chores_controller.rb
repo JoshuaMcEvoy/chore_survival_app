757058a92f7228b4c51e76c96d48d2a48be2f9b6
Joshua McEvoy <joshuamcevoy5@gmail.com>
2017-08-29 10:16:55 +1000

    user can create chore

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,13 +1,42 @@
 class ChoresController < ApplicationController
+
+  before_action :check_if_logged_in, :only => [:edit]
+
   def index
+    @chores = Chore.all
   end
 
   def new
+    @chore = Chore.new
+  end
+
+  def create
+    @chore = Chore.create chore_params
+    @current_user.chores << @chore
+    if
+      redirect_to chores_index_path
+    else
+      redirect_to chores_new_path
+    end
   end
 
   def edit
+    @chore = Chore.find params[:id]
+  end
+
+  def update
+    chore = Chore.find_by :id => session[:chore_id] if session[:chore_id].present?
+    session[:chore_id] = nil unless @current_chore.present?
+    chore = Chore.find params[:id]
+    chore.update chore_params
+    redirect_to root_path
   end
 
   def show
+  end
+
+  private
+  def chore_params
+    params.permit(:name, :description, :reward)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'chores/index'
   get 'chores/new'
+  post 'chores/new' => 'chores#create'
   get 'chores/edit'
   get 'chores/show'
+
+
+
 
   root :to => 'pages#home'
   resources :users
